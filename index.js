03750ed5eefdb19e1002c2366372949abad8083d
Hung Nguyen <nguyenmanhhungpro15@gmail.com>
2025-01-16 13:34:07 -0500

    add comments and clean up

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Toggles the responsive class on the navigation bar.
+ */
 function myFunction() {
   var nav = document.getElementById("myTopnav");
   if (nav.className === "topnav") {
@@ -6,6 +9,11 @@ function myFunction() {
     nav.className = "topnav";
   }
 }
+
+/**
+ * Sets the clicked element as active and removes the active class from the previously active element.
+ * @param {Event} e - The event object.
+ */
 function setActive(e) {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) {
@@ -13,16 +21,26 @@ function setActive(e) {
   }
   e.target.classList.add("active");
 }
+
+/**
+ * Blurs the navigation bar by resetting its class to "topnav" after a short delay.
+ */
 function blurFunction() {
   var nav = document.getElementById("myTopnav");
   setTimeout(() => {
     nav.className = "topnav";
   }, 10);
 }
+
+/**
+ * Validates the contact form and displays an error message if validation fails.
+ * If validation passes, displays a success alert.
+ */
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
